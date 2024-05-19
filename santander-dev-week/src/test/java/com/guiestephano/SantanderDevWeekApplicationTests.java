a1eea18796e734c9e4b9ab48c13dce61ab282daf
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-05-19 20:44:49 -0300

    Iniciando o projeto Santander Dev Week

---
+++ santander-dev-week/src/test/java/com/guiestephano/SantanderDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guiestephano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
